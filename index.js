d335f9f9506da104b43b1d217157b616c78c8c3a
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-06-04 17:32:14 +0530

    Added ErrorHandling for sync and async req

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ const express = require('express')
 const app = express()
 
 const logger = require('morgan')
+const ErrorHandler = require('./utils/ErrorHandler')
+const { generatedErrors } = require('./middlewares/errors')
 app.use(logger('tiny'))
 
 app.use('/', require('./routes/indexRoutes'))
+
+
+//error handling 
+app.all('*',(req,res,next)=>{
+    next(new ErrorHandler(`Requested URL Not Found ${req.url}`,404))
+})
+
+app.use(generatedErrors)
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`);
---
+++ middlewares/errors.js
@@ -0,0 +1,8 @@
+exports.generatedErrors = (err,req,res,next)=>{
+    const statuscode = err.statuscode || 500;
+    res.status(statuscode).json({
+        message:err.message,
+        errName:err.name,
+        //stack:err.stack
+    })
+}
